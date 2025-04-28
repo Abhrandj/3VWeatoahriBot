b503a86f9176601d0140df3cdbb6607a89d2ae38
Abhrandj <abhrandj@gmail.com>
2025-04-28 03:42:53 +0000

    Ignore test_telegram.py from GitHub repo

--- test_telegram.py
+++
@@ -1,7 +0,0 @@
-# test_telegram.py
-
-from core.telegram import send_telegram_message
-
-if __name__ == "__main__":
-    test_message = "✅ Test message berhasil dikirim dari TradingBot (via test_telegram.py)"
-    send_telegram_message(test_message)
